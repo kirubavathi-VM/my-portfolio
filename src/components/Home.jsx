518fae223fb877f1c098046ad2b2b01993daf809
VM-Kirubavathi <kirubavathi@tito.org.in>
2024-10-28 10:39:25 +0530

    my portfolio

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ import { Container, Row, Col, Card } from 'react-bootstrap';
 const Home = () => {
     return (
         <Container fluid className='m-0 p-0' id="home">
-            <div className='d-flex flex-column justify-content-center align-items-center vh-100 home pt-5'>
+            <div className='d-flex flex-column justify-content-center align-items-center vh-100 home'>
                 <Card className='home'>
                     <Card.Body>
                     <Row className="align-items-end">
